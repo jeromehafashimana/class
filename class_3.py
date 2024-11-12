b14d1272f8e15121f0fab0125a24ae5a35535a42
jerome <jeromehafasha@gmail.com>
2024-11-12 12:21:41 +0200

    Mise à jour du fichier avec les dernières modifications

--- class_3.py
+++ class_3.py
@@ -1,14 +1,13 @@
-class Produit:
-    def __init__(self, prix):
-        self._prix = prix
+class RechercheBinaire:
     
-    @property
-    def prix(self):
-        return self._prix
-    
-    @prix.setter
-    def prix(self, nouveau_prix):
-        if nouveau_prix > 0:
-            self._prix = nouveau_prix
-        else:
-            raise ValueError("Le prix doit être positif")
+    def rechercher(liste, cible):
+        gauche, droite = 0, len(liste) - 1
+        while gauche <= droite:
+            milieu = (gauche + droite) // 2
+            if liste[milieu] == cible:
+                return milieu
+            elif liste[milieu] < cible:
+                gauche = milieu + 1
+            else:
+                droite = milieu - 1
+        return -1
