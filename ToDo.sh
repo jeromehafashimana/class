45d8e5e9e78d9d030dbfc49b736a6eb1c0be5d97
jerome <jeromehafasha@gmail.com>
2024-11-21 14:24:55 +0200

    Mise à jour du fichier avec les dernières modifications

--- ToDo.sh
+++ ToDo.sh
@@ -1,1 +1,16 @@
 # Utiliser le module *requests* et afficher les donnees
+
+import requests
+
+
+url = "https://exemple.com/api/donnees"
+
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+    
+    print(response.json())
+else:
+    print(f"Erreur {response.status_code} : Impossible de récupérer les données.")
